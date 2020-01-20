dfa49dde890a771b1c85c1b47740bee05b85771b
Marco Köpcke <marco.koepcke@tudock.de>
2020-01-20 12:01:48 +0100

    Version Bump to 0.5.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ with open(path.join(this_directory, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='riptide-mission-control',
-    version='0.3.1',
+    version='0.5.0',
     packages=find_packages(),
     package_data={
         'tornadoql': ['static/graphiql.html'],
@@ -20,7 +20,7 @@ setup(
     long_description_content_type='text/x-rst',
     url='https://github.com/Parakoopa/riptide-proxy/',
     install_requires=[
-        'riptide-lib >= 0.3, < 0.4',
+        'riptide-lib >= 0.5, < 0.6',
         'tornado >= 6.0',
         'Click >= 7.0',
         'graphene >= 2.0, < 3.0',
